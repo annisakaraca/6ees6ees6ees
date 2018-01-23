1305ab4f15872dbbb4b020b67a5dfbc7a4af6e21
Annisa <annisakaraca@gmail.com>
2018-01-23 13:21:31 -0800

    Implement various Bee classes in ES6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
-  // TODO..
-};
+class HoneyMakerBee extends Bee {
+  constructor(age, job, color, food, eat, honeyPot) {
+    super(color, eat)
+    this.age = 10;
+    this.color = 'yellow';
+    this.honeyPot = 0;
+    this.job = 'make honey';
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+  giveHoney() {
+    this.honeyPot--;
+  }
+}
